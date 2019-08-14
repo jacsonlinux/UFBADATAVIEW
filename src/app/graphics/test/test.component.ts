3b65d375760e1a546ec7e298bd136fca3016ff7d
Jacson S. Correa <jacsonlinux@gmail.com>
2019-08-14 10:36:28 -0300

    ...

--- src/app/graphics/test/test.component.ts
+++ src/app/graphics/test/test.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import * as d3 from 'd3';
 
+import * as scale from 'd3-scale';
+
 import {AngularFireStorage} from '@angular/fire/storage';
 import {finalize} from 'rxjs/operators';
 import {Observable} from 'rxjs';
@@ -18,6 +20,9 @@ export class TestComponent implements OnInit {
   // uploadPercent: Observable<number>;
   // downloadURL: Observable<string>;
   uid: string;
+  dimensions: Array<string>;
+  url = 'https://gist.githubusercontent.com/jacsonlinux/da7cf481a61b93d6' +
+    '482b5ada7a508691/raw/bf8d27362d0636ac63c1513233cac56787a08916/iris.csv';
 
   constructor(
     private angularFireStorage: AngularFireStorage,
@@ -138,29 +143,112 @@ export class TestComponent implements OnInit {
       });
   }*/
 
-  // https://gist.githubusercontent.com/jacsonlinux/da7cf481a61b93d6482b5ada7a508691/raw/bf8d27362d0636ac63c1513233cac56787a08916/iris.csv
+  /*drawChart() {
+    const m = [100, 10, 10, 60];
+    const w = 1000 - m[1] - m[3];
+    const h = 300 - m[0] - m[2];
 
+    const x = d3.scaleBand().rangeRound([0, w]);
+    const y = {};
+    const dragging = {};
 
-  coord() {
+    const line = d3.line();
 
-    d3.csv('https://gist.githubusercontent.com/jacsonlinux/da7cf4' +
-      '81a61b93d6482b5ada7a508691/raw/bf8d27362d0636' +
-      'ac63c1513233cac56787a08916/iris.csv')
-      .then(data => {
+    const svg = d3.select('#test').append('svg:svg')
+      .attr('width', w + m[1] + m[3])
+      .attr('height', h + m[0] + m[2])
+      .append('svg:g')
+      .attr('transform', 'translate(' + m[3] + ',' + m[0] + ')');
 
-        const keys = data.columns.slice(1);
+    const data = [{
+      'A': 200,
+      'B': 3000,
+      'C': 2300,
+      'D': 4320,
+      'E': 1230,
+      'F': 7400,
+      'G': 1431,
+      'H': 1400,
+      'I': 4300,
+      'J': 6750
+    }, {
+      'A': 1002,
+      'B': 2003,
+      'C': 2773,
+      'D': 3432,
+      'E': 1673,
+      'F': 740,
+      'G': 1231,
+      'H': 1900,
+      'I': 3000,
+      'J': 675
+    }];
 
-        console.log(keys);
+    x.domain(this.dimensions = d3.keys(data[0]).filter(d => {
+      if (d === 'name') { return false; }
+      if (d === 'Plant' || d === 'Chemical' || d === 'Pathway' || d === 'Gene' || d === 'Disease') {
+        y[d] = d3.scaleOrdinal()
+          .domain(data.map(p => p[d]))
+          .range([h, 0]);
+      } else {
+        y[d] = d3.scaleLinear()
+          .domain(d3.extent(data, p => +p[d]))
+          .range([h, 0]);
+      }
+      return true;
+    }));
 
-      }).catch();
+    svg.append('svg:g')
+      .attr('class', 'background')
+      .selectAll('path')
+      .data(data)
+      .enter().append('svg:path')
+      .attr('d', path.bind(this));
 
+    svg.append('svg:g')
+      .attr('class', 'foreground')
+      .selectAll('path')
+      .data(data)
+      .enter().append('svg:path')
+      .attr('d', path.bind(this));
 
+    const g = svg.selectAll('.dimension')
+      .data(this.dimensions)
+      .enter().append('svg:g')
+      .attr('class', 'dimension')
+      .attr('transform', d => 'translate(' + x(d) + ')')
+
+    g.append('svg:g')
+      .attr('class', 'axis')
+      .each(function(d) {
+        d3.select(this).call(d3.axisLeft(y[d]));
+      })
+      .append('svg:text')
+      .attr('text-anchor', 'middle')
+      .attr('y', -50)
+      .attr('x', -10)
+      .text(String);
+
+    function position(d) {
+      const v = dragging[d];
+      return v == null ? x(d) : v;
+    }
+
+    function transition(g) {
+      return g.transition().duration(500);
+    }
+
+    function path(d) {
+      return line(this.dimensions.map(p => [position(p), y[p](d[p])]));
+    }
+  }*/
+
+  chart() {
+    d3.csv(`${this.url}`).then(data => {
+      console.log(data);
+    });
   }
 
-
-
-
-
-  ngOnInit() { this.coord();  }
+  ngOnInit() { this.chart();  }
 
 }
