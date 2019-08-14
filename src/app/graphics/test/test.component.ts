3bee7d5545cdd0cb17a2eac97aad3a5794a96d85
Jacson S. Correa <jacsonlinux@gmail.com>
2019-08-14 15:45:21 -0300

    ...

--- src/app/graphics/test/test.component.ts
+++ src/app/graphics/test/test.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AuthenticationService } from '../../authentication/authentication.service';
 
 import * as d3 from 'd3';
 
-import * as scale from 'd3-scale';
+import {STATISTICS} from '../shared';
 
-import {AngularFireStorage} from '@angular/fire/storage';
-import {finalize} from 'rxjs/operators';
-import {Observable} from 'rxjs';
-import {AuthenticationService} from '../../authentication/authentication.service';
-import { svg } from 'd3';
+import * as d3Selection from 'd3-selection';
+import * as d3Scale from 'd3-scale';
+import * as d3Array from 'd3-array';
+import * as d3Axis from 'd3-axis';
+
 
 @Component({
   selector: 'app-test',
@@ -19,10 +21,24 @@ export class TestComponent implements OnInit {
 
   // uploadPercent: Observable<number>;
   // downloadURL: Observable<string>;
+  // dimensions: Array<string>;
+
   uid: string;
-  dimensions: Array<string>;
-  url = 'https://gist.githubusercontent.com/jacsonlinux/da7cf481a61b93d6' +
-    '482b5ada7a508691/raw/bf8d27362d0636ac63c1513233cac56787a08916/iris.csv';
+
+  url =
+    'https://gist.githubusercontent.com/' +
+    'jacsonlinux/da7cf481a61b93d6482b5ada7a5' +
+    '08691/raw/bf8d27362d0636ac63c1513233cac' +
+    '56787a08916/iris.csv';
+
+  private width: number;
+  private height: number;
+  private margin = {top: 20, right: 20, bottom: 30, left: 40};
+
+  private x: any;
+  private y: any;
+  private svg: any;
+  private g: any;
 
   constructor(
     private angularFireStorage: AngularFireStorage,
@@ -243,12 +259,63 @@ export class TestComponent implements OnInit {
     }
   }*/
 
-  chart() {
-    d3.csv(`${this.url}`).then(data => {
+  /**/
+
+  /**/
+
+  ngOnInit() {
+    /*d3.csv(`${this.url}`)
+    .then(data => {
       console.log(data);
-    });
+    });*/
+    this.initSvg();
+    this.initAxis();
+    this.drawAxis();
+    this.drawBars();
   }
 
-  ngOnInit() { this.chart();  }
+  private initSvg() {
+    this.svg = d3Selection.select('svg');
+    this.width = +this.svg.attr('width') - this.margin.left - this.margin.right;
+    this.height = +this.svg.attr('height') - this.margin.top - this.margin.bottom;
+    this.g = this.svg.append('g')
+      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+  }
+
+  private initAxis() {
+    this.x = d3Scale.scaleBand().rangeRound([0, this.width]).padding(0.1);
+    this.y = d3Scale.scaleLinear().rangeRound([this.height, 0]);
+    this.x.domain(STATISTICS.map((d) => d.letter));
+    this.y.domain([0, d3Array.max(STATISTICS, (d) => d.frequency)]);
+  }
+
+  private drawAxis() {
+    this.g.append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', 'translate(0,' + this.height + ')')
+      .call(d3Axis.axisBottom(this.x));
+    this.g.append('g')
+      .attr('class', 'axis axis--y')
+      .call(d3Axis.axisLeft(this.y).ticks(10, '%'))
+      .append('text')
+      .attr('class', 'axis-title')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '0.71em')
+      .attr('text-anchor', 'end')
+      .text('Frequency');
+  }
+
+  private drawBars() {
+    this.g.selectAll('.bar')
+      .data(STATISTICS)
+      .enter().append('rect')
+      .attr('class', 'bar')
+      .attr('x', (d) => this.x(d.letter) )
+      .attr('y', (d) => this.y(d.frequency) )
+      .attr('width', this.x.bandwidth())
+      .attr('height', (d) => this.height - this.y(d.frequency) );
+  }
+
 
 }
